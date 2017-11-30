96f6cacc0e95a3d4fd19b3a0ba23ce89c19fbee5
joelfolkesson <joelfolkessonn@gmail.com>
2017-11-30 13:20:17 +0100

    Update mainlab1.c

--- mainlab1.c
+++ mainlab1.c
@@ -98,70 +98,6 @@ int i;
 
   printf("Starting...\n");
 
-  //Example 0 (Note: LEDs only works with real robot)
- // bool led[8] = {1,1,1,1,1,1,1,1};  // eight leds
- // SetRingLED (led);
-
-
-//SetTargetSteps(1000, 1000);
-
-
-/* remove this line
-
-  //Example 1
-  //printf("\n Moving wheels for 1000 step counts\n");
-  SetTargetSteps(1000, 1000);
-  Sleep(3000);
-
-
-  //Example 2
-  printf("\n Moving wheels for -1000 step counts\n");
-  SetTargetSteps(-1000, -1000);
-  Sleep(3000);
-
-
-  //Example 2
-  printf("\n Moving wheels for 0 step counts\n");
-  SetTargetSteps(0, 0);
-  Sleep(3000);
-
-
-  //Example 2
-  printf("\n Moving right wheel for -1000 and left for 1000 step counts\n");
-  SetTargetSteps(-1000, 1000);
-  Sleep(3000);
-
-
-  //Example 3
-  printf("\n setting speed\n");
-  SetSpeed(500, 500);
-  Sleep(4000);
-  Stop();
-
-
-  //Example 4
-  printf("\n Setting linear and angular velocities\n");
-  SetPolarSpeed(50, RAD(-28));
-  for (i=0;i<10;i++)
-  {
-    Sleep(100);
-    GetPolarSpeed(&vlin, &vrot);
-    printf("(vlin,vrot)=(%.2lf,%.2lf)\n", vlin, DEG(vrot));
-  }
-  Stop();
-
-
-  //Example 5
-  for (i=0;i<1000;i++)
-  {
-    ir = GetIR();
-    printf("IR values: %4d, %4d, %4d, %4d, %4d, %4d, %4d, %4d\n",
-           ir.sensor[0], ir.sensor[1], ir.sensor[2], ir.sensor[3],
-           ir.sensor[4], ir.sensor[5], ir.sensor[6], ir.sensor[7]);
-  }
-
-remove this line*/
-
 Posture now;
 Steps prev = GetSteps();
 now.x=0;
@@ -205,29 +141,6 @@ while(1){
 		default:
 			break;
 		}
-
-
-
-	// printf("\n now.x:  %f\n",now.x);
-	// printf("\n now.y:  %f\n",now.y);
-	// printf("\n now.th: %f\n",now.th);
-	//
-	//
-	// if(now.x >= 50 && now.x <= 52){
-	// 	Turn    (90.0);
-	// 	SetSpeed(50,50);
-	// 	Sleep   (1000);
-	// }
-	// if(now.x <= 52){
-	// 	printf("\nWE ARE DONE!\n");
-	// 	Stop();
-	// 	break;
-	// }
-	//if(now.th >= 50 || now.x <= -50){
-	//	printf("Snurrat klart!");
-	//	Stop();
-	//	break;
-	//}
 
 }
 
